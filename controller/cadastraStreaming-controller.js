cf165991f863d3418d77a1a6fa4531a2039a320b
Matheus Abreu <theus.santosab@gmail.com>
2021-12-29 15:52:28 -0300

    modificando tela add

--- controller/cadastraStreaming-controller.js
+++ controller/cadastraStreaming-controller.js
@@ -1,47 +1,42 @@
-import { streamingService } from '../service/streamingVideos-service.js'
+import { streamingService } from "../service/streamingVideos-service.js";
 
-const btnFechaModal = document.querySelector("#btn_close_modal");
-        btnFechaModal.addEventListener("click", () => {
-            document.querySelector(".modal-overlay").classList.remove("active");
-        });
-
-        
-function getValues(){
+function getValues() {
     return {
-        nome: document.querySelector('[data-nome]').value,
-        genero: document.querySelector('[data-genero]').value,
-        tipo: document.querySelector('[data-tipo]').value,
-        plataforma: document.querySelector('[data-plataforma]').value
-    }
+        nome: document.querySelector("[data-nome]").value,
+        genero: document.querySelector("[data-genero]").value,
+        tipo: document.querySelector("[data-tipo]").value,
+        plataforma: document.querySelector("[data-plataforma]").value,
+    };
 }
 
 function validate() {
-    const { nome, genero, tipo, plataforma } = getValues()
-        
-        if (nome.trim() === "" || genero.trim() === "" || tipo.trim() === "" || plataforma.trim() === "") {
-            throw new Error("Por favor, preencha todos os campos")
-        }
+    const { nome, genero, tipo, plataforma } = getValues();
+
+    if (
+        nome.trim() === "" ||
+        genero.trim() === "" ||
+        tipo.trim() === "" ||
+        plataforma.trim() === ""
+    ) {
+        throw new Error("Por favor, preencha todos os campos");
+        // window.location.href = "../views/error.html";
+    }
 }
 
-const formulario = document.querySelector('[data-form]')
+const formulario = document.querySelector("[data-form]");
 
-formulario.addEventListener('submit', (evento) => {
-    evento.preventDefault()
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
 
     try {
-        validate()
-        const { nome, genero, tipo, plataforma } = getValues()
+        validate();
+        const { nome, genero, tipo, plataforma } = getValues();
         const addStreaming = async () => {
-            await streamingService.criaStreaming(nome, genero, tipo, plataforma)
-           
-        }
-        addStreaming()
-
-        // alert(document.querySelector(".modal-overlay").classList.add("active"))
-        
+            await streamingService.criaStreaming(nome, genero, tipo, plataforma);
+        };
+        addStreaming();
+        // window.location.href = "../views/success.html";
+    } catch (error) {
+        console.log(error);
     }
-    catch (error) {
-        console.log(error)
-    }
-
-})
+});
